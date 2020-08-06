5a9251a362bd7de14de9414a5f28a44148b8d592
Jasongrass <junjiegrass@qq.com>
2020-08-06 19:33:24 +0800

    优化对 十 的处理。

--- code/ChineseNumberConvert/Handlers/ChineseNumberConverter.cs
+++ code/ChineseNumberConvert/Handlers/ChineseNumberConverter.cs
@@ -6,6 +6,13 @@ namespace ChineseNumberConvert.Handlers
     {
         public long Convert(string str)
         {
+            str = str.Trim();
+            if (str.StartsWith("十", System.StringComparison.Ordinal))
+            {
+                // 在 “十” 前面补充 “一”
+                str = "一" + str;
+            }
+
             ChineseNumberConvertContext context = new ChineseNumberConvertContext(str);
 
             ArrayList tree = new ArrayList();
